9b2ba884fb057130e830684eddbb74fc7c870a07
Bogdan Salyp <bogdan@dotin.us>
2019-03-03 12:48:37 +0300

    Empty cpp file with empty functions

---
+++ src/chainTools.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+void hashMe() {
+    
+}
+
+
+void makeTransaction() {
+    
+}
+
+
+void updateState() {
+   
+}
+
+
+bool isValidTxn() {
+    return true;
+}
+
+
+void makeBlock() {
+
+}
+
+
+bool checkBlockHash() {
+    return true;
+}
+
+
+bool checkBlockValidity() {
+    return true;
+}
+
+
+bool checkChain() {
+    return true;
+}
+
+int main() {
+    return 0;
+}
