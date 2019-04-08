5914c31419d85191e6556129177beb261e3f54f2
Ibragim <ahtubcex@ya.ru>
2019-04-08 17:54:52 +0300

    added simple buttons library

---
+++ SFML-app/sfml-app/sfml-app/FormSFML.h
@@ -0,0 +1,186 @@
+//
+//  FormSFML.h
+//  sfml-app
+//
+//  Created by Ибрагим on 08/04/2019.
+//  Copyright © 2019 Ибрагим Мамадаев. All rights reserved.
+//
+
+#ifndef FormSFML_h
+#define FormSFML_h
+
+#include <string>
+
+using namespace std;
+using namespace sf;
+
+namespace form {
+    Font None;                // Шрифт первичная инициализация
+    
+    class Label {
+    public:
+        string text;    //Текст
+    protected:
+        double x;        //Координата x
+        double y;        //Координата y
+        
+        Text TXT;        //Отображаемый текст
+        
+    public:
+        Label(double _x = 0, double _y = 0, string _text ="Текст")
+        {
+            None.loadFromFile("ttf/None.ttf");            //передаем нашему шрифту файл шрифта
+            
+            x = _x;
+            y = _y;
+            text = _text;                                // загружает текст
+            TXT.setFont(None);                            // загружаем фрифт
+            TXT.setCharacterSize(20);                     // в пикселях, а не точках!
+            TXT.setColor(Color::White);                    // устанавливаем цвет выводимого текста
+            TXT.setPosition(x,y);                        // позиция текста
+        }
+        Text displayText()
+        {
+            TXT.setString(text);                        // загружаем в выводимый текст загружаемый текст
+            return TXT;
+        }
+    };
+    
+    class Button : public Label {
+    protected:
+        double width;        // Ширина Кнопки
+        double height;        // Высота Кнопки
+        RectangleShape BOX;    // Создаем прямоугольник с размером width, height
+    public:
+        Button(double _x = 0, double _y = 0, double _width = 150, double _height = 30, string _text ="Кнопка")
+        {
+            None.loadFromFile("ttf/None.ttf");            //передаем нашему шрифту файл шрифта
+            
+            x = _x;
+            y = _y;
+            width = _width;
+            height = _height;
+            text = _text;                                // загружает текст
+            TXT.setFont(None);                            // загружаем фрифт
+            TXT.setCharacterSize(20);                     // в пикселях, а не точках!
+            TXT.setColor(Color::White);                    // устанавливаем цвет выводимого текста
+            TXT.setPosition(x,y);                        // позиция текста
+            
+            BOX.setSize(Vector2f(width, height));        // размер кнопки
+            BOX.setPosition(x,y);                        // координаты кнопки
+            BOX.setFillColor(sf::Color(133, 133, 133));    // цвет кнопки
+            BOX.setOutlineThickness(2);                    // Контур X пикселей
+            BOX.setOutlineColor(Color(66, 66, 66));        // Цвет контура
+        }
+        //Проверка нажатия на кнопку
+        bool select(Vector2i _mouse)
+        {
+            if((_mouse.x > x && _mouse.x < x + width) && (_mouse.y > y && _mouse.y < y + height)){
+                return  true;
+            } else {
+                return false;
+            }
+        }
+        //Вывод Текста
+        Text displayText()
+        {
+            TXT.setString(text);  // загружаем в выводимый текст загружаемый текст
+            return TXT;
+        }
+        //Вывод кнопки
+        RectangleShape displayButton()
+        {
+            return BOX;
+        }
+        
+        
+    };
+    
+    class Input : public Button{
+    protected:
+        bool focus;
+    public:
+        Input(double _x = 0, double _y = 0, double _width = 150, double _height = 30, string _text ="")
+        {
+            x = _x;
+            y = _y;
+            width = _width;
+            height = _height;
+            text = _text;                                // загружает текст
+            focus = false;                                // фокус поля ввода
+            
+            TXT.setFont(None);                            // загружаем фрифт
+            TXT.setCharacterSize(20);                     // в пикселях, а не точках!
+            TXT.setColor(Color::Black);                    // устанавливаем цвет выводимого текста
+            TXT.setPosition(x,y);                        // позиция текста
+            
+            BOX.setSize(Vector2f(width, height));        // размер поля ввода
+            BOX.setPosition(x,y);                        // координаты поля ввода
+            BOX.setFillColor(Color::White);    // цвет кнопки
+        }
+        //метод .reText( char ) изменяет текст внутри класса Типа данных Input
+        void reText(char _tmp)
+        {
+            text.erase(text.size()-1);
+            
+            if(_tmp != 8){
+                text += _tmp;
+            } else {
+                if(text.size() > 0){
+                    text.erase(text.size()-1);
+                }
+                
+            }
+            text += "|";
+            TXT.setString(text);
+        }
+        
+        // Проверка Фокуса Input
+        bool select()
+        {
+            return focus;
+        }
+        
+        // Установка Фокуса Input
+        bool select(Vector2i _mouse)
+        {
+            if((_mouse.x > x && _mouse.x < x + width) && (_mouse.y > y && _mouse.y < y + height)){     //Если нажат клаиша над объектом Input...
+                focus = true;                                                                           // Фокус true
+                text += "|";                                                                        // В конец строки добаляем | (что бы понимать что input в фокусе)
+            } else {                                                                                //...Иначе если нажатие произошло не над объектом, то...
+                if(text.size() > 0){                                                                // проверка последнего символа(иначе вылетает)
+                    if(text[text.size()-1] == 124){                                                    // если символ | ...
+                        text.erase(text.size()-1);                                                        // ... то удаляем его
+                    }
+                }
+                focus = false;                                                                        // устанавливаем фокус false
+            }
+            return focus;
+        }
+        
+        //Вывод Текста
+        Text displayText()
+        {
+            TXT.setString(text);                        // загружаем в выводимый текст загружаемый текст
+            return TXT;
+        }
+        
+        //Вывод фона Input
+        RectangleShape displayBOX()
+        {
+            return BOX;
+        }
+        
+        //Для того, что бы считать текст с поля Ipnut необходимо пременить  readText() который возвращает string
+        string readText()
+        {
+            if(text[text.size()-1] == 124){     // удаляем (если есть) символ |
+                text.erase(text.size()-1);
+            }
+            return text;
+        }
+    };
+    
+} /* namespace form */
+
+#endif /* FormSFML_h */
--- SFML-app/sfml-app/sfml-app/main.cpp
+++ SFML-app/sfml-app/sfml-app/main.cpp
@@ -13,11 +13,12 @@
 // application bundle. To get the path to these resources, use the helper
 // function `resourcePath()` from ResourcePath.hpp
 //
-
+#include <iostream>
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
 // Here is a small helper for you! Have a look.
+#include "FormSFML.h"
 #include "ResourcePath.hpp"
 
 int main(int, char const**)
@@ -28,6 +29,10 @@ int main(int, char const**)
     // Unresizable main window
     sf::RenderWindow window;
     window.create(sf::VideoMode(600, 570), "Fake app!", sf::Style::Close);
+    
+    // button from library
+    form::Button button3(270, 350, 90, 20, "Connect");
+
     
     // Setting ellipse shape
     sf::CircleShape ellipse(30.f);
@@ -59,9 +64,11 @@ int main(int, char const**)
         
         return EXIT_FAILURE;
     }
-    sf::Text text("fake coin app", font, 60);
+    sf::Text text("fake coin app", font, 42);
     text.setFillColor(sf::Color::White);
     
+    sf::Text buttonText("button", font, 30);
+    buttonText.setFillColor(sf::Color::Black);
     // Load a music to play
     //    sf::Music music;
     //    if (!music.openFromFile(resourcePath() + "nice_music.ogg")) {
@@ -96,19 +103,37 @@ int main(int, char const**)
         window.draw(sprite);
         
         // Draw the string
-        text.setPosition(120 , 0);
+        
+        sf::Text buttonText("button", font, 24);
+        buttonText.setFillColor(sf::Color::Black);
+        
+        buttonText.setFillColor(sf::Color::White);
+        buttonText.setString("Login");
+        buttonText.setPosition(500, 5);
+        window.draw(buttonText);
+        buttonText.setString("Settings");
+        buttonText.setPosition(500, 40);
+        window.draw(buttonText);
+        buttonText.setString("Button");
+        buttonText.setFillColor(sf::Color::Black);
+
+        text.setPosition(45 , 15);
         window.draw(text);
         
-        //draw the shape
+        //draw shapes
         for(int i=0; i<5; i++){
             ellipse.setPosition(120 + 80 * i, 100);
             rectangle.setPosition(120 + 80 * i, 200);
+            buttonText.setPosition(80 + 100 * i , 300);
+            window.draw(buttonText);
             window.draw(ellipse);
             window.draw(rectangle);
-
-
         }
-
+        
+        // draw button
+        window.draw(button3.displayButton());
+        window.draw(button3.displayText());
+        
         // Update the window
         window.display();
     }
